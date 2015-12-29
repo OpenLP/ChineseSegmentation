043ba6bc98947aeb8b007f5a8ca2eaf9c02720ca
DGideas <dgideas@outlook.com>
2015-12-29 16:10:04 +0800

    Create cs.cpp

---
+++ cs.cpp
@@ -0,0 +1,17 @@
+//无语料库的中文分词，C++实现
+//author:@DGideas,@sunlanchang
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<vector>
+#include<locale>
+using namespace std;
+int main(int argc,char *argv[])
+{
+        setlocale(LC_ALL, ""); //更改字符集为UTF-8
+        wstring str=L"十四是十四，四十是四十。十四不是四十，四十不是十四。"; //待分词的字符串
+        wstring symbol=L"，。！—；【】：《》"; //特殊符号
+        //wcin>>cn;
+        wcout<<a<<endl;
+        return 0;
+}
