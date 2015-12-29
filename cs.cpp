e4bf9e11df3686e1cac08cb71e47e2b5769e927a
DGideas <dgideas@outlook.com>
2015-12-30 06:19:59 +0800

    minor change

--- cs.cpp
+++ cs.cpp
@@ -8,21 +8,22 @@
 using namespace std;
 int main(int argc,char *argv[])
 {
-        setlocale(LC_ALL, ""); //更改字符集为UTF-8
-        wstring str=L"十四是十四，四十是四十。十四不是四十，四十不是十四。"; //待分词的字符串
-        wstring symbol=L"，。！—；【】：《》"; //特殊符号
-		wcout << symbol.size() << endl;
-        //wcout<<str<<endl;
-		for (int i=0; i<symbol.size();  i++)//删除标点符号
+	setlocale(LC_ALL, ""); //更改字符集为UTF-8
+	wstring str=L"十四是十四，四十是四十。十四不是四十，四十不是十四。"; //待分词的字符串
+	wstring symbol=L"，。！—；【】：《》"; //特殊符号
+	vector<wstring> split; //分割后的字符串
+	//wcout<<str<<endl;
+	for (int i=0;i<symbol.size();i++)//删除标点符号
+	{
+		for (int j=0;j<str.size();j++)
 		{
-			for (int j=0; j<str.size(); j++)
+			if (symbol[i]==str[j])
 			{
-				if (symbol[i] == str[j])
-				{
-					str[j] =L' ';
-				}
+				str[j] =L' ';
 			}
 		}
-		wcout << str << endl;
-        return 0;
+	}
+	
+	wcout<<str<<endl;
+	return 0;
 }
